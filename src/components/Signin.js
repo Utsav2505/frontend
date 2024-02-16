6a7b582904d5f71b1efb90cb02a8d16659aef6b5
Utsav <utsav2005maji@gmail.com>
2024-02-16 20:47:34 +0530

    loging  running but not going on server

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -9,26 +9,39 @@ export default function Sign(){
     const login = useGoogleLogin({
         onSuccess: (codeResponse) => setUser(codeResponse),
         onError: (error) => console.log('Login Failed:', error)
+        
     });
 
-    useEffect(
-        () => {
-            if (user) {
-                axios
-                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((res) => {
-                        setProfile(res.data);
-                    })
-                    .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
+    // useEffect(
+    //     () => {
+    //         if (user) {
+    //             axios
+    //                 .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+    //                     headers: {
+    //                         Authorization: `Bearer ${user.access_token}`,
+    //                         Accept: 'application/json'
+    //                     }
+    //                 })
+    //                 .then((res) => {
+    //                     setProfile(res.data);
+    //                 })
+    //                 .catch((err) => console.log(err));
+    //         }
+    //     },
+    //     [ user ]
+    // );
+
+    useEffect(() => {
+        if (user) {
+          // Fetch Gmail labels after successful authentication
+          axios
+            .get(`http://localhost:3001/getGmailLabels?accessToken=${user.access_token}`)
+            .then((res) => {
+              console.log('Gmail Labels:', res.data);
+            })
+            .catch((err) => console.log('Error fetching Gmail labels:', err));
+        }
+      }, [user]);
 
     // log out function to log the user out of google and set the profile array to null
     const logOut = () => {
@@ -57,12 +70,3 @@ export default function Sign(){
         </div>
     );
 }
-
-// return <GoogleLogin
-//     onSuccess={credentialResponse => {
-//       console.log(credentialResponse);
-//     }}
-//     onError={() => {
-//       console.log('Login Failed');
-//     }}
-//   />;
